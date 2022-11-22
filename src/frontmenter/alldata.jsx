877b3290c00c8b6a7e4b9cc92cf55e46c4453a41
adkshishir <98789290+adkshishir@users.noreply.github.com>
2022-11-22 10:21:02 -1200

    submit

---
+++ src/frontmenter/alldata.jsx
@@ -0,0 +1,35 @@
+const daily = [
+  {
+    key: 1,
+    title: "Work",
+    current: 5,
+    previous: 7,
+  },
+  { key: 2, title: "Play", current: 1, previous: 2 },
+  { key: 3, title: "Study", current: 0, previous: 1 },
+  { key: 4, title: "Exercise", current: 1, previous: 1 },
+  { key: 5, title: "Social", current: 1, previous: 3 },
+  { key: 6, title: "Self Care", current: 0, previous: 1 },
+];
+const weekly = [
+  { key: 1, title: "Work", current: 32, previous: 36 },
+  { key: 2, title: "Play", current: 10, previous: 8 },
+  { key: 3, title: "Study", current: 4, previous: 7 },
+  { key: 4, title: "Exercise", current: 4, previous: 5 },
+  { key: 5, title: "Social", current: 5, previous: 10 },
+  { key: 6, title: "Self Care", current: 2, previous: 2 },
+];
+
+const monthly = [
+  { key: 1, title: "Work", current: 103, previous: 128 },
+  { key: 2, title: "Play", current: 23, previous: 29 },
+  { key: 3, title: "Study", current: 13, previous: 19 },
+
+  { key: 4, title: "Exercise", current: 11, previous: 18 },
+
+  { key: 5, title: "Social", current: 21, previous: 23 },
+
+  { key: 6, title: "Self Care", current: 7, previous: 11 },
+];
+export default daily;
+export { monthly, weekly };
